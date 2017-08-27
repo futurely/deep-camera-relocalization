b1098cff86e4ddaa8d6160aae6f813ba503a6cfe
Kai Li <kaili_futurely@163.com>
2017-08-28 03:49:50 +0800

    Fix batch validation metrics

--- train.py
+++ train.py
@@ -1,22 +1,19 @@
 import math
 import os
 
-print 'from'
 from posenet import GoogLeNet as PoseNet
 from tensorflow.python.training import training_util
 from tqdm import tqdm
 
-print 'tensorflow'
 import tensorflow as tf
 import numpy as np
 
-print 'from local'
 from data import get_data, gen_data_batch
 from net_builder import build_posenet, add_pose_loss
 
-print 'global'
 batch_size = 48
 max_iterations = 30000
+display_interval = 20
 save_interval = 1000
 validation_interval = 1000
 # Set this path to your data_file data_dir
@@ -24,6 +21,7 @@ data_dir = '/home/user/Datasets/camera_relocalization/KingsCollege'
 train_data_file = 'dataset_train.txt'
 test_data_file = 'dataset_train.txt'
 model_path = '/home/user/Datasets/tensorflow/models/mobilenet/mobilenet_v1_1.0_224_2017_06_14/mobilenet_v1_1.0_224.ckpt'
+output_file = 'checkpoint/posenet_mobilenet.ckpt'
 restore_global_step = False
 debug = False
 
@@ -82,8 +80,6 @@ def main():
   checkpoint = tf.train.latest_checkpoint('.')
   if checkpoint is None:
     checkpoint = model_path
-  output_file = 'PoseNet.ckpt'
-  results = np.zeros((len(test_data_source.images), 2))
 
   with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
     # Load the data
@@ -103,46 +99,53 @@ def main():
 
     train_data_batch_generator = gen_data_batch(train_data_source, batch_size)
     test_data_batch_generator = gen_data_batch(test_data_source, batch_size)
+    num_test_images = len(test_data_source.images)
+    num_test_batches = (num_test_images + batch_size - 1) / batch_size
     for i in range(max_iterations):
       np_images, np_poses_x, np_poses_q = next(train_data_batch_generator)
       feed = {images: np_images, poses_x: np_poses_x, poses_q: np_poses_q}
 
       sess.run(opt, feed_dict=feed)
       np_loss = sess.run(loss, feed_dict=feed)
-      if i % 20 == 0:
+      if i > 0 and i % display_interval == 0:
         print('Iteration: ' + str(i) + '\n\t' + 'Loss is: ' + str(np_loss))
       if i > 0 and i % save_interval == 0:
         saver.save(sess, output_file, global_step=global_step)
         print('Intermediate file saved at: ' + output_file)
       if i > 0 and i % validation_interval == 0:
         print 'Validating'
-        for j in tqdm(range(len(test_data_source.images))):
+        results = np.zeros((len(test_data_source.images), 2))
+        for j in tqdm(range(num_test_batches)):
           np_image, np_poses_x, np_poses_q = next(test_data_batch_generator)
           feed = {images: np_image}
-
-          pose_q = np.asarray(test_data_source.poses[j][3:7])
-          pose_x = np.asarray(test_data_source.poses[j][0:3])
           predicted_x, predicted_q = sess.run([p3_x, p3_q], feed_dict=feed)
-
-          pose_q = np.squeeze(pose_q)
-          pose_x = np.squeeze(pose_x)
           predicted_q = np.squeeze(predicted_q)
           predicted_x = np.squeeze(predicted_x)
+
+          batch_start = batch_size * j
+          batch_end = min(batch_start + batch_size, num_test_images)
+          pose_q = np.asarray(
+              test_data_source.poses[batch_start:batch_end])[:, 3:7]
+          pose_x = np.asarray(
+              test_data_source.poses[batch_start:batch_end])[:, 0:3]
+          pose_q = np.squeeze(pose_q)
+          pose_x = np.squeeze(pose_x)
+          predicted_q = predicted_q[batch_start:batch_end]
+          predicted_x = predicted_x[batch_start:batch_end]
 
           #Compute Individual Sample Error
           q1 = pose_q / np.linalg.norm(pose_q)
           q2 = predicted_q / np.linalg.norm(predicted_q)
-          d = abs(np.sum(np.multiply(q1, q2)))
+          d = abs(np.sum(np.multiply(q1, q2), axis=1))
           theta = 2 * np.arccos(d) * 180 / math.pi
-          error_x = np.linalg.norm(pose_x - predicted_x)
-          results[j, :] = [error_x, theta]
-          print 'Validation iter:  ', j, '  Error XYZ (m):  ', error_x, '  Error Q (degrees):  ', theta
+          error_x = np.linalg.norm(pose_x - predicted_x, axis=1)
+          results[batch_start:batch_end, :] = np.column_stack((error_x, theta))
+        median_result = np.median(results, axis=0)
+        print 'Median error ', median_result[0], 'm  and ', median_result[
+            1], 'degrees.'
 
     saver.save(sess, output_file)
     print('Intermediate file saved at: ' + output_file)
-    median_result = np.median(results, axis=0)
-    print 'Median error ', median_result[0], 'm  and ', median_result[
-        1], 'degrees.'
 
 
 if __name__ == '__main__':
