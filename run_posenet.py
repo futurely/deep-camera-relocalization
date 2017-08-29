4a2698ed979bd20d4221547d13a9a9b76c4405ea
Kai Li <kaili_futurely@163.com>
2017-08-30 02:48:25 +0800

    Fix computing metric

--- run_posenet.py
+++ run_posenet.py
@@ -11,8 +11,11 @@ import numpy as np
 from data import get_data, gen_data_batch
 from net_builder import build_posenet, add_pose_loss
 
+max_num_train_images = -1
+max_num_test_images = -1
 batch_size = 48
 max_iterations = 30000
+max_iterations = 1
 display_interval = 20
 save_interval = 1000
 test_interval = 1000
@@ -24,15 +27,11 @@ model_path = '/home/user/Datasets/tensorflow/models/mobilenet/mobilenet_v1_1.0_2
 checkpoint_dir = 'checkpoint'
 output_checkpoint_dir = 'checkpoint'
 checkpoint_file = 'posenet_mobilenet.ckpt'
-restore_global_step = False
+train = True
+test = True
 test_first = True
 
-test_only = True
-test_only = False
-
 debug = False
-
-#debug = True
 
 
 def should_load(name):
@@ -43,21 +42,22 @@ def should_load(name):
   return True
 
 
+def load_data(data_dir, data_file, max_num_images=-1):
+  data_path = os.path.join(data_dir, data_file)
+  if max_num_images >= 0:
+    data_source = get_data(data_path, data_dir, max_num_images)
+  else:
+    data_source = get_data(data_path, data_dir)
+  num_images = len(data_source.images)
+  num_batches = (num_images + batch_size - 1) / batch_size
+  print 'num_images', num_images, 'batch_size', batch_size, 'num_batches', num_batches
+  return data_source
+
+
 def main():
   images = tf.placeholder(tf.float32, [batch_size, 224, 224, 3])
   poses_x = tf.placeholder(tf.float32, [batch_size, 3])
   poses_q = tf.placeholder(tf.float32, [batch_size, 4])
-  print 'get_data'
-  train_data_path = os.path.join(data_dir, train_data_file)
-  if debug:
-    train_data_source = get_data(train_data_path, data_dir, 10)
-  else:
-    train_data_source = get_data(train_data_path, data_dir)
-  test_data_path = os.path.join(data_dir, test_data_file)
-  if debug:
-    test_data_source = get_data(test_data_path, data_dir, 10)
-  else:
-    test_data_source = get_data(test_data_path, data_dir)
 
   print 'build_posenet'
   net = build_posenet(images, 'mobilenet')
@@ -99,6 +99,12 @@ def main():
     checkpoint = model_path
   print 'checkpoint', checkpoint
 
+  if train:
+    train_data_source = load_data(data_dir, train_data_file,
+                                  max_num_train_images)
+  if test:
+    test_data_source = load_data(data_dir, test_data_file, max_num_test_images)
+
   with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
     # Load the data
     sess.run(init)
@@ -114,10 +120,13 @@ def main():
       restorer.restore(sess, checkpoint)
     print('Model restored from file: %s' % checkpoint)
 
-    train_data_batch_generator = gen_data_batch(train_data_source, batch_size)
-    test_data_batch_generator = gen_data_batch(test_data_source, batch_size)
-    num_test_images = len(test_data_source.images)
-    num_test_batches = (num_test_images + batch_size - 1) / batch_size
+    if train:
+      train_data_batch_generator = gen_data_batch(train_data_source,
+                                                  batch_size)
+    if test:
+      test_data_batch_generator = gen_data_batch(test_data_source, batch_size)
+      num_test_images = len(test_data_source.images)
+      num_test_batches = (num_test_images + batch_size - 1) / batch_size
     iter = -1
     for i in range(max_iterations):
       if (i > 0 or test_first) and i % test_interval == 0:
@@ -140,20 +149,20 @@ def main():
               test_data_source.poses[batch_start:batch_end])[:, 0:3]
           pose_q = np.squeeze(pose_q)
           pose_x = np.squeeze(pose_x)
-          predicted_q = predicted_q[batch_start:batch_end]
-          predicted_x = predicted_x[batch_start:batch_end]
+          predicted_q = predicted_q[:batch_end - batch_start]
+          predicted_x = predicted_x[:batch_end - batch_start]
 
           #Compute Individual Sample Error
-          q1 = pose_q / np.linalg.norm(pose_q)
-          q2 = predicted_q / np.linalg.norm(predicted_q)
-          d = abs(np.sum(np.multiply(q1, q2), axis=1))
+          pose_q /= np.linalg.norm(pose_q)
+          predicted_q /= np.linalg.norm(predicted_q)
+          d = abs(np.sum(np.multiply(pose_q, predicted_q), axis=1))
           theta = 2 * np.arccos(d) * 180 / math.pi
           error_x = np.linalg.norm(pose_x - predicted_x, axis=1)
           results[batch_start:batch_end, :] = np.column_stack((error_x, theta))
         median_result = np.median(results, axis=0)
         print 'Median error ', median_result[0], 'm  and ', median_result[
             1], 'degrees.'
-      if not test_only:
+      if train:
         np_images, np_poses_x, np_poses_q = next(train_data_batch_generator)
         feed = {images: np_images, poses_x: np_poses_x, poses_q: np_poses_q}
 
